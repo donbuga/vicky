bfccfcc257be9ecbd57d7f55f551373d25d79511
donbuga <bugadj@gmail.com>
2026-08-27 20:28:27 -0400

    feat: agregar juego Historias de Darwin

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,4 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-export default defineConfig({base:'./',plugins:[react()],build:{rollupOptions:{input:{home:'index.html',adventure:'evaluacion-13-agosto.html',sailorMoon:'lecturas-sailor-moon.html'}}}});
+export default defineConfig({base:'./',plugins:[react()],build:{rollupOptions:{input:{home:'index.html',adventure:'evaluacion-13-agosto.html',sailorMoon:'lecturas-sailor-moon.html',darwin:'historias-de-darwin.html'}}}});
